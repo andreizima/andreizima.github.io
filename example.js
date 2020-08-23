76dcfaf7f0adc30606f86c73d28fda9bb212422c
Andrei <zimaandrei9@gmail.com>
2020-08-23 13:19:21 +0300

    Added a test button

---
+++ example.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    function(){
+        $('#addbox').on('click',
+            function (){
+                var newBox = '<div class="box"></div>';
+                $("h3").remove();
+                $(".content").append(newBox);
+            }
+        );
+    }
+)
